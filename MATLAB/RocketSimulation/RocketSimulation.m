4b6d060f0aa8aa1db256034bcd30355159500169
f <f@ll.com>
2020-12-26 08:57:18 +0300

    Edit
    Wrong equation is removed and rocket picture added.

--- MATLAB/RocketSimulation/RocketSimulation.m
+++ MATLAB/RocketSimulation/RocketSimulation.m
@@ -11,13 +11,13 @@ G = -9.81;
 
 delta = 0.1;
 t = 0:delta:20;
-F = linspace(12,4,length(t));
+F = linspace(12,6,length(t));
 
 PVA = zeros(length(t),3);
 
 % Calculating PVA
 for m=1:length(t)
-    a0 = a0 + delta*((F(m)/M) +G);
+    a0 = delta*((F(m)/M) +G);   % a0 + removed
     v0 = v0 + a0*delta;
     x0 = x0 + v0*delta + (a0*delta^2)/2;
     PVA(m,1) = x0;
